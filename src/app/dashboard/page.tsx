744150dc2d8376f0c514222aee70a06c31db3dec
svnoliveira <srkuran@gmail.com>
2024-01-24 17:44:52 -0300

    feat: redirecionamento de páginas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,13 @@
+'use client'
 import { DashboardCard } from "@/components/DashboardCard";
+import { userStore } from "@/stores/userStore";
+import { redirect } from "next/navigation";
+
+const { loading, userData } = userStore((state) => state)
+
+  if (!userData) {
+    redirect('/login');
+  }
 
 export default function DashboardPage() {
     return (
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,16 @@
+'use client'
 import { RegisterForm } from "@/components/RegisterForm";
+import { userStore } from "@/stores/userStore";
+import { redirect } from "next/navigation";
 
 
 export default function RegisterPage() {
+  const { loading, userData } = userStore((state) => state)
+
+  if (userData) {
+    redirect('/dashboard');
+  }
+
     return (
       <main>
         <h1>Cadastrar</h1>
