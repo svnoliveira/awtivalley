68c6799881636f3039866f03a090c61e83ba05a5
svnoliveira <srkuran@gmail.com>
2024-02-22 17:47:31 -0300

    fix: ajustado loading de pontos

--- src/app/registro-de-ponto/page.tsx
+++ src/app/registro-de-ponto/page.tsx
@@ -2,15 +2,26 @@
 import { PontoForm } from "@/components/PontoForm";
 import { Loading } from "@/fragments/Loading";
 import { GlobalStyle } from "@/globalStyles/globalstyle";
+import { registroStore } from "@/stores/registroDePonto";
 import { userStore } from "@/stores/userStore";
 import { redirect } from "next/navigation";
+import { useEffect } from "react";
 
 export default function PontoPage() {
   const { loading, userData } = userStore((state) => state);
-
+  const { loadPontos } = registroStore((state) => state)
   if (!userData) {
     redirect("/login");
   }
+
+  useEffect(() => {
+    const loadList = () => {
+      if (userData) {
+        loadPontos();
+      }
+    };
+    loadList();
+  }, []);
 
   return (
     <>
