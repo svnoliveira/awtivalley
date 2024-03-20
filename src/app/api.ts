a9f1efab0721ab07ea8dcd19ad5b7eaafd8b4469
svnoliveira <srkuran@gmail.com>
2024-03-20 18:24:24 -0300

    feat: ajustado endereço da API

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://awtivalley-api.onrender.com/api/";
-// export const API_BASE_URL = "http://127.0.0.1:8000/api/";
+// export const API_BASE_URL = "https://awtivalley-api.onrender.com/api/";
+export const API_BASE_URL = "http://apicapital.centromedicoawti.com.br/api/";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
