916aa98a15e4e06f228d155e9e4b8dc4aa4d02bd
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-03-25 23:18:23 -0300

    voltado para a API de produção

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "https://awtivalley-api.onrender.com/api/";
-//export const API_BASE_URL = "http://apicapital.centromedicoawti.com.br/api/";
+// export const API_BASE_URL = "https://awtivalley-api.onrender.com/api/";
+export const API_BASE_URL = "http://apicapital.centromedicoawti.com.br/api/";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
