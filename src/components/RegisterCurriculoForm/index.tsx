beb1092b93eecd7fe1c774fc158dafee2a155002
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-03-27 18:47:07 -0300

    Update index.tsx

--- src/components/RegisterCurriculoForm/index.tsx
+++ src/components/RegisterCurriculoForm/index.tsx
@@ -11,19 +11,30 @@ import Image from "next/image";
 import Link from "next/link";
 
 export const RegisterCurriculoForm = () => {
- 
-  const {
-    register,
-    handleSubmit,
-
-  } = useForm<TRegisterCurriculoValues>({
+  const { register, handleSubmit } = useForm<TRegisterCurriculoValues>({
     resolver: zodResolver(registerCurriculoSchema),
   });
-  const { push } = useRouter();
+  const router = useRouter();
+
+  const onSubmit = (data: TRegisterCurriculoValues) => {
+    console.log(data);
+    router.push('/outra-pagina');
+  };
 
   return (
     <StyledSection>
-          CADASTRARasdas
+      <Link href="/">Voltar</Link>
+      <StyledForm onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("nome")} placeholder="Nome" required />
+        <input type="text" {...register("passaporte")} placeholder="Passaporte" required />
+        <input type="text" {...register("telefone", { pattern: /\d{3}-\d{3}/ })} placeholder="Telefone (###-###)" required />
+        <input type="text" {...register("experiencia")} placeholder="Experiência" required list="experienciaOptions" />
+        <input type="text" {...register("disponibilidadeEntrevista")} placeholder="Disponibilidade para entrevista" required />
+        <input type="text" {...register("disponibilidadeTrabalho")} placeholder="Disponibilidade para trabalho" required />
+        <input type="file" {...register("imagem")} accept="image/*" required />
+        <button type="submit">Cadastrar</button>
+      </StyledForm>
     </StyledSection>
   );
 };
+
