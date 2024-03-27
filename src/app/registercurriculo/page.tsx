f3c1a0f4940ef21ec71fcdab11889cf62049c8fa
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-03-26 22:16:00 -0300

    curriculo

--- src/app/registercurriculo/page.tsx
+++ src/app/registercurriculo/page.tsx
@@ -7,24 +7,14 @@ import { checkUserRole } from "@/utils/operations";
 import { redirect } from "next/navigation";
 
 export default function RegisterCurriculoForm() {
-  const { loading, userData } = userStore((state) => state);
-
-  if (checkUserRole(userData?.user) === false) {
-    redirect("/dashboard");
-  }
-
   return (
     <>
       <GlobalStyle />
-      {loading ? (
-        <Loading />
-      ) : (
         <>
           <main>
             <RegisterCurriculoForm />
           </main>
         </>
-      )}
     </>
   );
 }
