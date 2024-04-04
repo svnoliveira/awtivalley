5f9ac895bf3514c42d69e41af1dc99928cd7163e
tiagosucci <34147114+tiagosucci@users.noreply.github.com>
2024-04-04 16:32:49 -0300

    voltado ao anterior

--- src/components/AdminBonusMenu/index.tsx
+++ src/components/AdminBonusMenu/index.tsx
@@ -339,7 +339,7 @@ return (
                         <input
                           type="checkbox"
                           checked={bonusEnabled[user.funcao]}
-                          onChange={() => toggleBonus(user.nome)}
+                          onChange={() => toggleBonus(user.funcao)}
                         />
                         <span className="slider round"></span>
                       </label>
