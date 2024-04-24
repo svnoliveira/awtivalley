567ec91a5edd5ac375e1bf716a2d5d1ebfb621e5
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-04-24 18:32:34 -0300

    globalstyles

---
+++ src/globalStyles/CursosNav/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const CursosNav = styled.nav`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    margin-top: 20px;
+    gap: 15px;
+`
