2b329e1bd9443dc5b5e1e850706a4fe895399fd1
tiagosucci <34147114+tiagosucci@users.noreply.github.com>
2024-04-25 10:20:56 -0300

    correção envio de curriculo

--- src/components/RegisterCurriculoForm/index.tsx
+++ src/components/RegisterCurriculoForm/index.tsx
@@ -11,8 +11,9 @@ import Link from "next/link";
 import axios from 'axios';
 
 export const RegisterCurriculoForm = () => {
-  const [image, setImage] = useState<File | null>(null); 
+  const [image, setImage] = useState<File | null>(null);
   const [successMessage, setSuccessMessage] = useState<string>("");
+
   const router = useRouter();
 
   const {
@@ -41,6 +42,10 @@ export const RegisterCurriculoForm = () => {
 
   const onSubmit = async (data: TRegisterCurriculoValues) => {
     try {
+      if (!image) {
+        console.error('Por favor, selecione um arquivo.');
+        return; // Não envia o formulário se o arquivo não foi selecionado
+      }
       let message = `:heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign::heavy_minus_sign:\n` +
       `# <a:alerta:1223357938912067675> <@&1192311823211442207> | <@&1127323287060496408> <a:alerta:1223357938912067675>\n\n` +
       `# :bookmark_tabs: **Novo Currículo cadastrado: ** :bookmark_tabs:\n\n` +
@@ -57,9 +62,8 @@ export const RegisterCurriculoForm = () => {
 
       console.log('Mensagem de webhook enviada com sucesso!');
 
-      if (image) {
-        const formData = new FormData();
-        formData.append('file', image);
+      const formData = new FormData();
+      formData.append('file', image);
 
         await axios.post('https://discord.com/api/webhooks/1223430514342367443/KzoSeJ-iMWeo1Cj0Vip8DLbDjbZ35_L73vB-gQdYWYFIKziZofYwNgftr4bdbs2ECytK', formData, {
           headers: {
@@ -69,94 +73,90 @@ export const RegisterCurriculoForm = () => {
             file: image.name
           }
         });
-
+  
         console.log('Imagem enviada com sucesso!');
+        
+        reset(); // Limpar o formulário após o envio da mensagem e do arquivo
+        setSuccessMessage("Currículo cadastrado com sucesso!");
+      } catch (error) {
+        console.error('Erro ao enviar mensagem de webhook:', error);
       }
-      
-      reset();
-
-      setSuccessMessage("Currículo cadastrado com sucesso!");
-    } catch (error) {
-      console.error('Erro ao enviar mensagem de webhook:', error);
-    }
+    };
+  
+    const checkError = () => {
+      return (
+        !!errors.nome ||
+        !!errors.passaporte ||
+        !!errors.telefone ||
+        !!errors.experiencia ||
+        !!errors.disponibilidadeEntrevista ||
+        !!errors.disponibilidadeTrabalho
+      );
+    };
+  
+    return (
+      <StyledSection>
+        {successMessage && <div style={{ backgroundColor: 'lightgreen', padding: '10px', marginBottom: '10px' }}>{successMessage}</div>}
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+          <Link href="/">Voltar</Link>
+          <Image
+            src="/cma-logo-black.png"
+            alt="Logo awti valley"
+            width={300}
+            height={500}
+          />
+          <span>Cadastrar de Currículos</span>
+          <FormInput
+            type="text"
+            register={register("nome")}
+            error={errors.nome}
+          >
+            Nome
+          </FormInput>
+          <FormInput
+            type="text"
+            register={register("passaporte")}
+            error={errors.passaporte}
+          >
+            Passaporte
+          </FormInput>
+          <FormInput
+            type="text"
+            register={register("telefone", { pattern: /\d{3}-\d{3}/ })}
+            error={errors.telefone}
+          >
+            Telefone (###-###)
+          </FormInput>
+          <FormInput
+            type="text"
+            register={register("experiencia")}
+            error={errors.experiencia}
+          >
+            Experiência
+          </FormInput>
+          <FormInput
+            type="text"
+            register={register("disponibilidadeEntrevista")}
+            error={errors.disponibilidadeEntrevista}
+          >
+            Disponibilidade para entrevista
+          </FormInput>
+          <FormInput
+            type="text"
+            register={register("disponibilidadeTrabalho")}
+            error={errors.disponibilidadeTrabalho}
+          >
+            Disponibilidade para trabalho
+          </FormInput>
+          <StyledSelectWrapper>
+            <input type="file" onChange={handleImageChange} accept="image/*" />
+          </StyledSelectWrapper>
+          <br />
+          {image === null && <p style={{ color: 'red' }}>Por favor, selecione um arquivo.</p>}
+          <StyledSubmitButton $error={checkError()} type="submit">
+            CADASTRAR
+          </StyledSubmitButton>
+        </StyledForm>
+      </StyledSection>
+    );
   };
-
-  const checkError = () => {
-    if (
-      errors.nome ||
-      errors.passaporte ||
-      errors.telefone ||
-      errors.experiencia ||
-      errors.disponibilidadeEntrevista ||
-      errors.disponibilidadeTrabalho
-    ) {
-      return true;
-    }
-    return false;
-  };
-
-  return (
-    <StyledSection>
-      {successMessage && <div style={{ backgroundColor: 'lightgreen', padding: '10px', marginBottom: '10px' }}>{successMessage}</div>}
-      <StyledForm onSubmit={handleSubmit(onSubmit)}>
-        <Link href="/">Voltar</Link>
-        <Image
-          src="/cma-logo-black.png"
-          alt="Logo awti valley"
-          width={300}
-          height={500}
-        />
-        <span>Cadastrar de Currículos</span>
-        <FormInput
-          type="text"
-          register={register("nome")}
-          error={errors.nome}
-        >
-          Nome
-        </FormInput>
-        <FormInput
-          type="text"
-          register={register("passaporte")}
-          error={errors.passaporte}
-        >
-          Passaporte
-        </FormInput>
-        <FormInput
-          type="text"
-          register={register("telefone", { pattern: /\d{3}-\d{3}/ })}
-          error={errors.telefone}
-        >
-          Telefone (###-###)
-        </FormInput>
-        <FormInput
-          type="text"
-          register={register("experiencia")}
-          error={errors.experiencia}
-        >
-          Experiência
-        </FormInput>
-        <FormInput
-          type="text"
-          register={register("disponibilidadeEntrevista")}
-          error={errors.disponibilidadeEntrevista}
-        >
-          Disponibilidade para entrevista
-        </FormInput>
-        <FormInput
-          type="text"
-          register={register("disponibilidadeTrabalho")}
-          error={errors.disponibilidadeTrabalho}
-        >
-          Disponibilidade para trabalho
-        </FormInput>
-        <StyledSelectWrapper>
-        <input type="file" onChange={handleImageChange} accept="image/*" />
-        </StyledSelectWrapper>
-       <br></br>
-        <StyledSubmitButton $error={checkError()} type="submit">
-          CADASTRAR
-        </StyledSubmitButton>
-      </StyledForm>
-    </StyledSection>
-  );
-};
