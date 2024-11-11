3ebade4a54038267cf32e6cedc9ea513cf76ba45
Tiago Succi <34147114+tiagosucci@users.noreply.github.com>
2024-11-11 18:10:56 -0300

    Prova

---
+++ src/components/ProvaModCondMedConflitos/index.tsx
@@ -0,0 +1,38 @@
+import {   StyledSection,
+  StyledContainer,
+  StyledList,
+  StyledListItem } from "./style";
+import { userStore } from "@/stores/userStore";
+
+export const ProvaModCondMedConflitos = () => {
+  const user = userStore((state) => state.userData?.user);
+
+  return (
+    <StyledSection>
+      <StyledContainer>
+        <center>
+          <h1>Curso de Modulação</h1>
+        </center>
+        <hr />
+        <hr />
+        <StyledList>          
+          <StyledListItem>
+            <center><h2>🗣️ Modulação</h2></center>
+            <p>
+              <strong>Aula Teórico:</strong>
+              <li>➜ Acesse a Sala de aula clicando <b><a href="https://classroom.google.com/c/NzI5MDE0NTE4MjU0?cjc=nfvld6v">AQUI</a></b>, participe da turma e asiste a aula e faça a prova.</li>
+              <li>➜ Após finalizar a prova, volte para a classe vai clicar em Ver instuções e marcar como Concluída.</li>
+            </p>
+            <p>
+              <strong>Observações:</strong>
+            </p>
+            <ul>
+              <li>➜ Qualquer problema encontrado, sinalizar para a equipe de Cursos.</li>
+              <li>➜ Os Resultados serão informados na aba de ✅┃𝗥𝗲𝘀𝘂𝗹𝘁𝗮𝗱𝗼𝘀 do Discord.</li>
+            </ul>
+          </StyledListItem>
+        </StyledList>
+      </StyledContainer>
+    </StyledSection>
+  );
+};
